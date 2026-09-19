2b62e8dc076f824f955f1b81623e11ea5b60d0f3
roohalla41-blip <roohalla41@gmail.com>
2026-09-19 15:34:26 +0330

    Create MainActivity.java
    افزودن صفحه اصلی برنامه

---
+++ app/app/src/main/app/src/main/java/com/base44/apk/MainActivity.java
@@ -0,0 +1,28 @@
+package com.base44.apk;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    private static final String APP_URL = "https://madar-app.base44.app/";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(APP_URL);
+    }
+}
