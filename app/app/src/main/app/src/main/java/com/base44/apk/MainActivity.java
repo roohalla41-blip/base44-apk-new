b15fa3ff45e8c80e90090c3c0bbc91c251e7a349
roohalla41-blip <roohalla41@gmail.com>
2026-09-21 19:11:23 +0330

    Update MainActivity.java
    جدیدترین

--- app/app/src/main/app/src/main/java/com/base44/apk/MainActivity.java
+++ app/app/src/main/app/src/main/java/com/base44/apk/MainActivity.java
@@ -9,52 +9,53 @@ import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
 
-    private static final String APP_URL =
-            "https://practical-pure-quran-path.base44.app/";
+private static final String APP_URL =
+        "https://practical-pure-quran-path.base44.app/";
 
-    private WebView webView;
+private WebView webView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-        webView = new WebView(this);
-        setContentView(webView);
+    webView = new WebView(this);
+    setContentView(webView);
 
-        WebSettings settings = webView.getSettings();
+    WebSettings settings = webView.getSettings();
 
-        settings.setJavaScriptEnabled(true);
-        settings.setDomStorageEnabled(true);
-        settings.setDatabaseEnabled(true);
-        settings.setLoadWithOverviewMode(true);
-        settings.setUseWideViewPort(true);
-        settings.setBuiltInZoomControls(false);
-        settings.setDisplayZoomControls(false);
+    settings.setJavaScriptEnabled(true);
+    settings.setDomStorageEnabled(true);
+    settings.setDatabaseEnabled(true);
+    settings.setLoadWithOverviewMode(true);
+    settings.setUseWideViewPort(true);
+    settings.setBuiltInZoomControls(false);
+    settings.setDisplayZoomControls(false);
 
-        CookieManager cookieManager = CookieManager.getInstance();
-        cookieManager.setAcceptCookie(true);
-        cookieManager.setAcceptThirdPartyCookies(webView, true);
+    CookieManager cookieManager = CookieManager.getInstance();
+    cookieManager.setAcceptCookie(true);
+    cookieManager.setAcceptThirdPartyCookies(webView, true);
 
-        webView.setWebViewClient(new WebViewClient());
+    webView.setWebViewClient(new WebViewClient());
 
-        webView.loadUrl(APP_URL);
+    webView.loadUrl(APP_URL);
+}
+
+@Override
+public void onBackPressed() {
+    if (webView != null && webView.canGoBack()) {
+        webView.goBack();
+    } else {
+        super.onBackPressed();
     }
+}
 
-    @Override
-    public void onBackPressed() {
-        if (webView != null && webView.canGoBack()) {
-            webView.goBack();
-        } else {
-            super.onBackPressed();
-        }
+@Override
+protected void onDestroy() {
+    if (webView != null) {
+        webView.stopLoading();
+        webView.destroy();
     }
+    super.onDestroy();
+}
 
-    @Override
-    protected void onDestroy() {
-        if (webView != null) {
-            webView.stopLoading();
-            webView.destroy();
-        }
-        super.onDestroy();
-    }
 }
